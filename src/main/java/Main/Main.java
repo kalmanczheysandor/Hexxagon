41e43020b2657fb8c4229be27839f59fedf32bfb
Kálmánchey Sándor <kalmanczheysandor@gmail.com>
2021-01-08 18:43:46 +0100

    Sublime is ahead

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -25,6 +25,7 @@ import org.slf4j.LoggerFactory;
 /**
  * The main class of the whole application.
  * ide a netbeans jon
+ * Ide meg a sublime jon
  * @author Sandor Kalmanchey
  * @version 2.0
  * @since 2.0
